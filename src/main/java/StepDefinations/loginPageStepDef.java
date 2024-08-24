e9ed66d55099f0519dee6bee0f46f27d24c5b64e
Ajit Shirsat <ajitshirsat2k8@gmail.com>
2024-08-24 18:17:05 +0530

    Updated cucumber scenario

---
+++ src/main/java/StepDefinations/loginPageStepDef.java
@@ -0,0 +1,31 @@
+package StepDefinations;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginPageStepDef {
+    @Given("I am on login page")
+    public void iAmOnLoginPage() {
+    }
+
+    @And("User enter username")
+    public void userEnterUsername() {
+        
+    }
+
+    @And("user enter password")
+    public void userEnterPassword() {
+        
+    }
+
+    @When("user able to see home page")
+    public void userAbleToSeeHomePage() {
+        
+    }
+
+    @Then("User check title")
+    public void userCheckTitle() {
+    }
+}
--- src/test/features/newtest.feature
+++ src/test/features/newtest.feature
@@ -1,8 +1,8 @@
 Feature: Login test
 
   Scenario: I login with valid credential
-    Given I navigate to "http://the-internet.herokuapp.com/login"
-    And I enter "tomsmith" into input field having xpath "//input[@id='username']"
-    And I enter "SuperSecretPassword!" into input field having id "password"
-    When I click on element having class "radius"
-    Then I wait 5 seconds for element having css "a[href='/logout']" to display
+    Given I am on login page
+    And User enter username
+    And user enter password
+    When user able to see home page
+    Then User check title
